be5e09cf6c5d903f5a1c3a97024361937a45af8a
Claire van Blerck <60234167+ClairevanBlerck@users.noreply.github.com>
2020-06-15 19:54:16 +0100

    Add game tutorials to portfolio site

---
+++ _site/games/gameboy-pacman/app.js
@@ -0,0 +1,231 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const width = 28; // 28 x 28 = 784 squares
+    const grid = document.querySelector ('.grid');
+    const scoreDisplay = document.getElementById ('score');
+    let score = 0;
+
+    const layout = [
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,0,1,
+        1,0,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,0,1,
+        1,0,0,0,0,0,0,1,1,1,0,0,0,1,1,0,0,0,1,1,1,0,0,0,0,0,0,1,
+        1,1,1,1,1,1,0,1,1,1,1,1,4,1,1,4,1,1,1,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+        4,4,4,4,4,4,0,4,4,4,1,2,2,2,2,2,2,1,4,4,4,0,4,4,4,4,4,4,
+        1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+        1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+        1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+        1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+        1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+        1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+        1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+        1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+        1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+        1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+    ]
+
+    const squares = []
+
+    //Legend
+    //0 - pac-dot
+    //1 - wall
+    //2 - ghost-lair
+    //3 - power-pellet
+    //4 - empty
+
+    // draw the grid and render it
+    function createGrid() {
+        for (let i=0; i < layout.length; i++) {
+            const square = document.createElement('div');
+            grid.appendChild(square);
+            squares.push(square);
+
+            //add layout to the board
+            if (layout[i] === 0) {
+                squares[i].classList.add('pac-dot', 'dot');
+            } else if (layout[i] === 1) {
+                squares[i].classList.add('wall');
+            } else if (layout[i] === 2) {
+                squares[i].classList.add('ghost-lair')
+            } else if (layout[i] === 3) {
+                squares[i].classList.add('power-pellet');
+            }
+        }
+    }
+    createGrid()
+
+    //starting position of pac-man
+    let pacmanCurrentIndex = 574;
+
+    squares[pacmanCurrentIndex].classList.add('pac-man');
+
+    //move pac-man
+    function movePacman(e) {
+
+        squares[pacmanCurrentIndex].classList.remove('pac-man');
+
+        switch(e.keyCode) {
+            // move up
+            case 38:
+                if(pacmanCurrentIndex - width >= 0 && !squares[pacmanCurrentIndex -width].classList.contains('wall') && !squares[pacmanCurrentIndex -width].classList.contains('ghost-lair')) pacmanCurrentIndex -= width;
+                
+                break;
+
+            // move right
+            case 39:
+                if(pacmanCurrentIndex % width <= width -1 && !squares[pacmanCurrentIndex +1].classList.contains('wall') && !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair')) pacmanCurrentIndex += 1;
+                // check if pacman is at the right-side exit
+                if ((pacmanCurrentIndex +1) === 392) {
+                    pacmanCurrentIndex = 364;
+                }
+                break;
+
+            // move down
+            case 40:
+                if(pacmanCurrentIndex + width <= layout.length && !squares[pacmanCurrentIndex +width].classList.contains('wall') && !squares[pacmanCurrentIndex +width].classList.contains('ghost-lair')) pacmanCurrentIndex += width;
+                break;
+
+            // move left
+            case 37:
+                if(pacmanCurrentIndex % width !== 0 && !squares[pacmanCurrentIndex -1].classList.contains('wall') && !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair')) pacmanCurrentIndex -= 1;
+                // check if pacman is at the left-side exit
+                if ((pacmanCurrentIndex -1) === 363) {
+                    pacmanCurrentIndex = 391;
+                }
+                break;
+        }
+
+        squares[pacmanCurrentIndex].classList.add('pac-man');
+
+        pacDotEaten()
+        powerPelletEaten()
+        gameOver()
+        checkForWin()
+
+    }
+    document.addEventListener('keyup', movePacman, pacDotEaten);
+
+    // what happens them pac-man collides with a pac-dot
+    function pacDotEaten() {
+        if (squares[pacmanCurrentIndex].classList.contains('pac-dot', 'dot')) {
+            score++;
+            scoreDisplay.innerHTML = score;
+            squares[pacmanCurrentIndex].classList.remove('pac-dot', 'dot');
+        }
+    }
+
+    //what happens when pac-man collides with a pac-dot
+    function powerPelletEaten() {
+        if (squares[pacmanCurrentIndex].classList.contains('power-pellet')) {
+            score+=10;
+            scoreDisplay.innerHTML = score;
+            squares[pacmanCurrentIndex].classList.remove('power-pellet');
+            ghosts.forEach(ghost => ghost.isScared = true);
+            setTimeout(unScare, 10000);
+        }
+    }
+
+    function unScare() {
+        ghosts.forEach(ghost => ghost.isScared = false);
+    }
+
+    // ghost template
+    class ghost {
+        constructor (className, startIndex, speed) {
+            this.className = className;
+            this.startIndex = startIndex;
+            this.speed = speed;
+            this.currentIndex = startIndex;
+            this.timerID = NaN;
+            this.isScared = false;
+        }
+    }
+
+    ghosts = [
+        new ghost('blinky', 348, 250),
+        new ghost('pinky', 376, 400),
+        new ghost('inky', 351, 300),
+        new ghost('clyde', 379, 500),
+    ]
+
+    //starting position of ghosts
+    ghosts.forEach(ghost => {
+        squares[ghost.currentIndex].classList.add(ghost.className);
+        squares[ghost.currentIndex].classList.add('ghost');
+    });
+
+    //moving ALL ghosts - random
+    ghosts.forEach(ghost => moveGhosts(ghost))
+
+    function moveGhosts(ghost) {
+        const directions = [-1, 1, width, -width];
+        let direction = directions[Math.floor(Math.random() * directions.length)];
+
+        ghost.timerID = setInterval(function() {
+            // prevent ghosts from moving into walls and another ghost
+           if (!squares[ghost.currentIndex + direction].classList.contains('wall') && !squares[ghost.currentIndex + direction].classList.contains('ghost')) {
+               // remove any trace of ghost
+               squares[ghost.currentIndex].classList.remove(ghost.className, 'ghost', 'scared-ghosts');
+               // change direction by 1
+               ghost.currentIndex += direction;
+               // redraw ghost
+               squares[ghost.currentIndex].classList.add(ghost.className, 'ghost');
+               } else direction = directions[Math.floor(Math.random() * directions.length)];
+
+            // add scared-ghosts class for when isScared = true
+            if (ghost.isScared) {
+                squares[ghost.currentIndex].classList.add('scared-ghosts');
+            }
+
+            // add pac-man ability to eat scared-ghosts
+            if (ghost.isScared && squares[ghost.currentIndex].classList.contains('pac-man')) {
+                // remove ghost
+                squares[ghost.currentIndex].classList.remove(ghost.className, 'ghost', 'scared-ghosts');
+                ghost.currentIndex = ghost.startIndex;
+                //add to score
+                score += 100;
+                scoreDisplay.innerHTML = score;
+                // respawn ghost
+                squares[ghost.currentIndex].classList.add(ghost.className, 'ghost');
+            }
+
+            // check to see if ghost moves into pac-man
+            gameOver()
+
+        //add unique speeds
+        }, ghost.speed)
+    } gameOver()
+
+    // check to see if pac-man moves into ghost
+    function gameOver() {
+        if (squares[pacmanCurrentIndex].classList.contains('ghost') &&
+          !squares[pacmanCurrentIndex].classList.contains('scared-ghosts')) {
+          ghosts.forEach(ghost => clearInterval(ghost.timerId))
+          document.removeEventListener('keyup', movePacman)
+          //setTimeout(function(){ alert("Game Over"); }, 200)
+          scoreDisplay.innerHTML = 'Game over';
+        }
+    }
+
+    //check for a win
+    function checkForWin() {
+        if (score >= 274) {
+          ghosts.forEach(ghost => clearInterval(ghost.timerId));
+          document.removeEventListener('keyup', movePacman);
+          //setTimeout(function(){ alert("You have WON!"); }, 500);
+          scoreDisplay.innerHTML = 'YOU WON!';
+        }
+      }
+
+})
---
+++ _site/games/gameboy-tetris/app.js
@@ -0,0 +1,324 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const width = 10;
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    let nextRandom = 0;
+    let timerId;
+    //controls
+    const startButton = document.querySelector ('#start-button');
+    const upButton = document.querySelector ('#up-button');
+    const rightButton = document.querySelector ('#right-button');
+    const leftButton = document.querySelector ('#left-button');
+    const downButton = document.querySelector ('#down-button');
+    const aButton = document.querySelector ('#a-button');
+    const bButton = document.querySelector ('#b-button');
+    let score = 0;
+    const scoreDisplay = document.querySelector('#score');
+    let level = 0;
+    const levelDisplay = document.querySelector('#level');
+    let lines = 0;
+    const linesDisplay = document.querySelector('#lines');
+    //tetromino display colours/images
+    let colors = ["l1", "l2", "z1", "z2", "t", "o", "i"];
+
+
+    //The Tetrominoes
+    const l1Tetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const l2Tetromino = [
+        [0, 1, width+1, width*2+1],
+        [width, width+1, width+2, 2],
+        [1, width+1, width*2+1, width*2+2],
+        [width, width*2, width+1, width+2]
+    ]
+
+    const z1Tetromino = [
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1],
+        [0, width, width+1, width*2+1],
+        [width+1, width+2, width*2, width*2+1]
+    ]
+
+    const z2Tetromino = [
+        [width, width+1, width*2+1, width*2+2],
+        [2, width+1, width+2, width*2+1],
+        [width, width+1, width*2+1, width*2+2],
+        [2, width+1, width+2, width*2+1]
+    ]
+
+    const tTetromino = [
+        [width, 1, width+1, width+2],
+        [1, width+1, width+2, width*2+1],
+        [width, width+1, width+2, width*2+1],
+        [width, 1, width+1, width*2+1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
+
+    const theTetrominoes = [l1Tetromino, l2Tetromino, z1Tetromino, z2Tetromino, tTetromino, oTetromino, iTetromino];
+    let currentPosition = 4;
+    let currentRotation = 0;
+    
+    //randomly select a Tetromino and it's first rotation
+    let random = Math.floor(Math.random()*theTetrominoes.length);
+    console.log(random)
+    let current = theTetrominoes[random][currentRotation];
+    console.log(colors[random])
+    console.log((colors[random]))
+    
+
+    //draw the Tetromino
+    function draw() {
+        current.forEach(index =>{
+            //squares[currentPosition + index].classList.add('tetromino');
+            squares[currentPosition + index].classList.add((colors[random]));
+        })
+    }
+
+    //undraw the Tetromino
+    function undraw() {
+        current.forEach(index => {
+            //squares[currentPosition + index].classList.remove('tetromino');
+            //squares[currentPosition + index].classList(colors[random]);
+            squares[currentPosition + index].classList = "";
+        })
+    }
+
+    //make the Tetromino move down the screen every second
+    //timerId = setInterval(moveDown, 1000)
+
+    //assign functions on keyCodes
+    /*function control(e) {
+        if(e.keyCode === 65) {
+            moveLeft();
+        } else if (e.keyCode === 68) {
+            moveRight();
+        } else if (e.keyCode == 83) {
+            moveDown();
+        } else if (e.keyCode === 39) {
+            rotateClockwise();
+        } else if (e.keyCode === 37) {
+            rotateCounterClockwise();
+        }
+    }*/
+
+    //add functionality to 'A' / right-arrow key
+    aButton.addEventListener('click', () => {
+        rotateClockwise();
+        }
+    )
+
+    //add functionality to 'B' / left-arrow key
+    bButton.addEventListener('click', () => {
+        rotateCounterClockwise();
+        }
+    )
+
+    //add functionality to the 'A' key
+    leftButton.addEventListener('click', () => {
+        moveLeft();
+        }
+    )
+
+    //add functionality to the 'D' key
+    rightButton.addEventListener('click', () => {
+        moveRight();
+        }
+    )
+    
+    //add functionality to the 'S' key
+    downButton.addEventListener('click', () => {
+        moveDown();
+        }
+        ),
+    downButton.addEventListener('', () => {
+        moveDownFast();
+        }
+    )
+
+    //movedown function
+    function moveDown() {
+        undraw();
+        currentPosition += width;
+        draw();
+        freeze();
+    }
+
+    //movedown function
+    function moveDownFast() {
+        moveDown();
+        timerId = setInterval(moveDown, 400);
+    }
+
+    //freeze function
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+            //start a new Tetromino falling
+            random = nextRandom;
+            nextRandom =  Math.floor(Math.random() * theTetrominoes.length);
+            current = theTetrominoes[random][currentRotation];
+            currentPosition = 4;
+            draw();
+            displayShape();
+            addScore();
+            addLevel();
+            gameOver();
+        }
+    }
+
+
+    //move the tetromino left, unless it is at the left edge or the index is taken
+    function moveLeft () {
+        undraw();
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)   
+        if(!isAtLeftEdge) currentPosition -=1;
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition +=1;
+        }
+        draw();
+    }
+
+    //move the tetromino right, unless it is at the right edge or the index is taken
+    function moveRight () {
+        undraw();
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+        if(!isAtRightEdge) currentPosition +=1;
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -=1;
+        }
+        draw();
+    }
+
+    //rotate the tetromino clockwise / to the right
+    function rotateClockwise() {
+        undraw();
+        currentRotation ++;
+        if(currentRotation === current.length) { //if the array limit has been reached proceed to 0
+            currentRotation = 0;
+        } 
+        current = theTetrominoes[random][currentRotation];
+        draw();
+    }
+
+    //rotate the tetromino counter-clockwise / to the left
+    function rotateCounterClockwise() {
+        undraw();
+        currentRotation --;
+        if(currentRotation = [0]) { //if the array limit has been reached proceed to index 3 - FIX THIS
+            currentRotation = [3];
+        }
+        current = theTetrominoes[random][currentRotation];
+        draw();
+    }
+
+    //show up-next tetromino in mini-grid display
+    const displaySquares = document.querySelectorAll('.mini-grid div');
+    const displayWidth = 4;
+    const displayIndex = 0;
+
+    //the Tetraminoes without rotation
+    const upNextTetrominoes = [
+        [1, displayWidth+1, displayWidth*2+1, 2], //l1Tetromino
+        [0, 1, displayWidth+1, displayWidth*2+1], //l2Tetromino
+        [0, displayWidth, displayWidth+1, displayWidth*2+1], //z1Tetromino
+        [displayWidth, displayWidth+1, displayWidth*2+1, displayWidth*2+2], //z2Tetromino
+        [displayWidth, 1, displayWidth+1, displayWidth+2], //tTetromino
+        [0, 1, displayWidth, displayWidth+1], //oTetromino
+        [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1], //iTetromino
+    ]
+
+    //display the shape in the mini-grid display
+    function displayShape() {
+        //remove any trace of a tetromino from the entire grid
+        displaySquares.forEach(square => {
+            //square.classList.remove('tetromino');
+            //square.classList.remove((colors[random]));
+            square.classList = "";
+        })
+        upNextTetrominoes[nextRandom].forEach( index => {
+            //displaySquares[displayIndex + index].classList.add('tetromino');
+            displaySquares[displayIndex + index].classList.add((colors[nextRandom]));
+        })
+    }
+
+    //add functionality to the start button
+    startButton.addEventListener('click', () => {
+        if (timerId) {
+            clearInterval(timerId);
+            timerId = null;
+        } else {
+            draw();
+            timerId = setInterval(moveDown, 1000);
+            nextRandom = Math.floor(Math.random()*theTetrominoes.length);
+            displayShape();
+        }
+    })
+
+    //add score
+    function addScore() {
+        for (let i = 0; i < 199; i +=width) {
+            const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+            if(row.every(index => squares[index].classList.contains('taken'))) {
+                score +=10;
+                scoreDisplay.innerHTML = score;
+                lines +=1;
+                linesDisplay.innerHTML = lines;
+                row.forEach(index => {
+                    //squares[index].classList.remove(colors[random]);
+                    //squares[index].classList.remove('taken');
+                    //squares[index].classList.remove('tetromino');
+                    squares[index].classList = "";
+                })
+                const squaresRemoved = squares.splice(i, width);
+                squares = squaresRemoved.concat(squares);
+                squares.forEach(cell => grid.appendChild(cell));
+            }
+        }
+    }
+
+    function addLevel() {
+        level = Math.floor(lines / 10);
+        levelDisplay.innerHTML = level;
+    }
+
+    //game over
+    function gameOver() {
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        scoreDisplay.innerHTML = 'end';
+        clearInterval(timerId);
+        }
+    }
+})
+
+/* Known bugs:
+1. Can i remove copy/paste pop-up from rotation buttons?
+2. #b-button - only rotates once, only occasionally, and not to currentRotation--
+3. rotate next to border - allows tetromino to be split by width
+4. right-button and left-button when colliding with taken tetrominos seems to be merging
+*/
+
+/* Missing features:
+1. Tetrominos - add stying for each shape
+2. Controls - add 'hold' down-button function to drop the tetromino to the highest 'taken' div
+3. Score - add complex scores for multi-line elimination and level-based multi-lne elimination
+4. Line elimination - add animation
+5. Score save - add top scores board
+*/
